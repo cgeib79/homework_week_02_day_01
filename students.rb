90f2188ff0847e91bb09c922a6def57952638ce1
Christian Geib <christian.geib79@gmail.com>
2018-07-24 06:44:55 +0100

    initial commit

---
+++ students.rb
@@ -0,0 +1,34 @@
+class Students
+
+  attr_reader :name, :cohort
+
+  attr_writer :name, :cohort
+
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  # def
+  #   get_name()
+  #   return @name
+  # end
+  #
+  # def
+  #   get_cohort()
+  #   return @cohort
+  # end
+
+  # def change_name(new_name)
+  #   @students.replace(new_name)
+  # end
+
+  def talk()
+    return ("I can talk")
+  end
+
+  def fav_language()
+    return ("I love Ruby")
+  end
+
+end
